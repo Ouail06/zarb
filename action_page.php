1d59cbeb7fef05667da567894e9c67540c3276a3
Ouail06 <9019064@student.zadkine.nl>
2023-02-06 16:11:37 +0100

    ididi

--- action_page.php
+++ action_page.php
@@ -1,13 +1,12 @@
-<html>
-    <?php
+<?php
 $a = array("yassin", "Ouail", "Bilal" );
-
-echo '<table border="1" width="200">';foreach($a as $name){ echo "<tr><td>$name</td></tr>";}echo "</table>";
-"<br>";
-echo date("l jS \of F Y h:i:s A") . "<br>";echo "<p>hello world</p> <br>";
-
+ echo '<table border="1" width="200">';
+  for ($i=0; $i < count($a); $i++) {
+    echo "<tr><td>{$a[$i]}</td><td>Rotterdam</td></tr>";
+}
+echo "</table>";"<br>";
+echo date("l jS \of F Y h:i:s A") . "<br>";
+echo "<p>hello world</p> <br>";
 echo $_POST['uname'];
 echo "<br>";
-echo $_POST['pwd'];
-?>
-</html>
+echo $_POST['pwd'];?>
