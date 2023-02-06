6f059a97d43a09e034b78c8fe9b3d4bd6f3f0f91
Ouail06 <9019064@student.zadkine.nl>
2023-02-06 16:02:19 +0100

    lioi

--- action_page.php
+++ action_page.php
@@ -1,7 +1,13 @@
-<?php
-echo "<p>hello world</p> <br>";
-var_dump($_POST);
+<html>
+    <?php
+$a = array("yassin", "Ouail", "Bilal" );
 
-echo $_POST ["Uname"]; 
-$username = $_POST['Uname'];
+echo '<table border="1" width="200">';foreach($a as $name){ echo "<tr><td>$name</td></tr>";}echo "</table>";
+"<br>";
+echo date("l jS \of F Y h:i:s A") . "<br>";echo "<p>hello world</p> <br>";
+
+echo $_POST['uname'];
+echo "<br>";
+echo $_POST['pwd'];
 ?>
+</html>
