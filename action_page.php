bfb8464ae6e0c18112b250383270dc42ed71abb0
Ouail06 <9019064@student.zadkine.nl>
2023-02-13 15:08:13 +0100

    hxjhvxjgS

--- action_page.php
+++ action_page.php
@@ -1,12 +1,27 @@
 <?php
-$a = array("yassin", "Ouail", "Bilal" );
- echo '<table border="1" width="200">';
-  for ($i=0; $i < count($a); $i++) {
-    echo "<tr><td>{$a[$i]}</td><td>Rotterdam</td></tr>";
-}
-echo "</table>";"<br>";
-echo date("l jS \of F Y h:i:s A") . "<br>";
-echo "<p>hello world</p> <br>";
-echo $_POST['uname'];
-echo "<br>";
-echo $_POST['pwd'];?>
+$a = array("ouail", "bilal", "yasine" );
+echo '<table border="1" width="200">';
+for ($i=0; $i < count($a); $i++) {
+   echo "<tr><td>{$a[$i]}</td><td>Rotterdam</td></tr>";}
+   echo "</table>";
+   echo date("") . "<br>";
+   echo date("l jS \of F Y h:i:s A") . "<br>";
+    echo "<p>hello world</p> <br>";
+   
+   $Uname = $_POST['Uname'];
+   $pwd = $_POST['pwd'];
+   
+   echo "Username: $Uname<br>";
+   echo "Password: $pwd";
+
+   echo '<table border="1" width="200>"';
+foreach ($_POST as $name){
+  //maak een rij
+  echo "<tr>";
+  echo "<td>";
+  echo $name;
+  echo "</td>";
+};
+
+  
+?>  
