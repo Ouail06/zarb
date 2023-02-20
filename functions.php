6e135d872af6405389335ac81fbafa20b2d90611
Ouail06 <9019064@student.zadkine.nl>
2023-02-20 16:29:06 +0100

    lnack sf

--- functions.php
+++ functions.php
@@ -1,39 +1,40 @@
 <?php
 
-function ConnectDb(){
-     try { $conn = new PDO("mysql:host=localhost;dbname=bieren.sql", "root", "");
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     echo "geslaagd";
-     }  
+function ConnectDb() {
+  try {
+      $conn = new PDO("mysql:host=localhost;dbname=bieren.sql", "root", "");
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn;
 
+  } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+  }
+}
 
-     catch (PDOException $e) 
-     { echo "Error: " . $e->getMessage();
-    }
-    return $conn;
-    }
-     
-     
-     function GetData($conn, $table){
-          GetData($conn, "bier");
+function GetData($conn, $table){
     $query = $conn->prepare("SELECT * FROM $table");
     $query->execute();
-    $result = $query->fetchAll (PDO:: FETCH_ASSOC);
-    return $result;
-     
-     }
-     function OvzBieren($conn) {
-          $result = GetData($conn, "bier");
+    $result = $query->fetchALL(PDO::FETCH_ASSOC);
+    return $result;}
 
-          echo "<table border=1px>";
-          foreach ($result as $data) {
-               echo"<tr>";
-               echo "<td>";  $data ["naam"]; "</td>";
-               echo "<td>";  $data ["alcohol"]; "</td>";
-               echo"</tr>";
-          }
-          echo '<table border="1">';
-
-     }
-     ?>
-    
+function OvzBieren() {
+    $conn = ConnectDb();
+    echo "<table border='1'>";
+    echo "<tr><th>id</th><th>bieren</th><th>Alcoholpercentage</th></tr>";
+  
+    try {
+       $rows = GetData($conn, "bier");
+        foreach ($rows as $row) {
+            echo "<tr>";
+            echo "<td>" . $row["biercode"] . "</td>";
+            echo "<td>" . $row["naam"] . "</td>";
+            echo "<td>" . $row["alcohol"] . "</td>";
+            echo "</tr>";
+        }
+  
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    echo "</table>";
+    $conn = null;
+  }
--- main_bieren.php
+++ main_bieren.php
@@ -3,27 +3,10 @@
 include 'functions.php';
 
 $conn = ConnectDb();
+
 var_dump($conn);
+  
 
-    
-OvzBieren($conn){
-    $conn = OpenDb();
-    
-     echo "<table border='1'>"; 
-    echo "<tr>
-    <th>id</th>
-    <th>bieren</th>
-    <th>Alcoholpercentage</th>
-    </tr>";
-    try { $stmt = $conn->prepare("SELECT * FROM bier"); $stmt->execute(); while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {echo "<tr>";
-         echo "<td>" . $row["biercode"] . "</td>";
-        echo "<td>" . $row["naam"] . "</td>";
-       echo "<td>" . $row["alcohol"] . "</td>";
-        echo "</tr>";
-     } } catch (PDOException $e) 
-     {echo "Error: " . $e->getMessage(); } echo "</table>";
-     $conn = null;
-    }
-
-    
+ OvzBieren($conn);
+ 
 ?>
